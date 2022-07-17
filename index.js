09f196953f998b98a8b78daa3cc75b877fce829f
Yashar Ghafarinejad <yasharg97@gmail.com>
2022-07-16 21:57:30 -0500

    desafio corregido

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+let carritoDeCompras = [];
+
+let contador = document.getElementById("contador");
+
 let contenedorProductos = document.getElementById("zonaDeProductos");
 
- 
+let panelCarrito = document.getElementById("panelCarrito");
 
 
 
@@ -19,10 +23,7 @@ let contenedorProductos = document.getElementById("zonaDeProductos");
 
 
 
-
-//logica ecommerce
-
-function cargarProductos(){
+function mostrarProductos(){
 
     raquetas.forEach(item => {
         let div = document.createElement("div");
@@ -31,11 +32,71 @@ function cargarProductos(){
         <div class="card-body">
             <h5 class="card-title">${item.nombre}</h5>
             <p class="class-text">${item.precio}</p>
-            <a href="#" class="boton-carrito">Agregar al Carrito</a>
+            <a href="#" id="producto${item.id}"class="boton-carrito">Agregar al Carrito</a>
         </div>`;
         contenedorProductos.appendChild(div);
-    })
-    
+
+        let btnAgregar = document.getElementById(`producto${item.id}`);
+        btnAgregar.addEventListener("click", () => {
+            agregarAlCarrito(item.id);
+        });
+        
+       
+    });
+
 }
 
-cargarProductos();
+mostrarProductos();
+function agregarAlCarrito(id){
+    
+
+    let productoAgregar = raquetas.find(element => element.id == id);
+    productoAgregar.cantidad = 1;
+    carritoDeCompras.push(productoAgregar);
+
+    mostrarCarrito(productoAgregar);
+    actualizarCarrito();
+
+    // console.log(carritoDeCompras);
+
+}
+
+function mostrarCarrito(productoAgregar){
+
+    let div = document.createElement("div");
+    div.className = "productoEnCarrito";
+    div.innerHTML = `<h5 class="card-title">${productoAgregar.nombre}</h5>
+    <p class="class-text">${productoAgregar.precio}</p>
+    <p id="${productoAgregar.id}">Und: ${productoAgregar.cantidad}</p>
+    <button id="eliminar${productoAgregar.id}">eliminar</button>`;
+    panelCarrito.appendChild(div);  
+
+    let btnEliminar = document.getElementById(`eliminar${productoAgregar.id}`);
+
+    btnEliminar.addEventListener("click", () => {
+        
+        carritoDeCompras = carritoDeCompras.filter(item => item.id != productoAgregar.id);
+
+        console.log(carritoDeCompras);
+
+        btnEliminar.parentElement.remove();
+
+        actualizarCarrito();
+        
+    })
+
+}
+
+function actualizarCarrito(){
+
+    contador.innerText = carritoDeCompras.length;
+
+
+    
+
+
+}
+
+function recuperar(){
+
+}
--- stock.js
+++ stock.js
@@ -1,10 +1,10 @@
 let raquetas = [
-    {nombre: "CLASH 98 V2.0", precio: "S/.989.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr074211u_1_clash_98_v2_rd_bl.png.cq5dam.web.1200.1200_1_1.jpeg"},
-    {nombre: "CLASH 108 V2.0", precio: "S/.969.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/70463b1ff005ad550922e9aee1aaa0df/w/r/wr074511u_1_clash_108_v2_rd_bl.png.cq5dam.web.1200.1200.jpeg"},
-    {nombre: "CLASH 100 V2.0", precio: "S/.969.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr074011u_1_clash_100_v2_rd_bl.png.cq5dam.web.1200.1200_1_1.jpeg"},
-    {nombre: "CLASH 100 PRO V2.0", precio: "S/:969.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr074011u_1_clash_100_v2_rd_bl.png.cq5dam.web.1200.1200_1_1.jpeg"},
-    {nombre: "CLASH 100UL V2.0", precio: "S/:889.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr074011u_1_clash_100_v2_rd_bl.png.cq5dam.web.1200.1200_1_1.jpeg"},
-    {nombre: "PRO STAFF 97L V13", precio: "S/.955.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr043811u_1_pro_staff_97_v13_2_1_1.jpeg"},
-    {nombre: "PRO STAFF 97 CV RAQUETA DE TENIS", precio: "S/. 839.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr043811u_1_pro_staff_97_v13_2_1_1.jpeg"},
-    {nombre: "PRO STAFF 97 CV RAQUETA DE TENIS", precio: "S/. 839.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr043811u_1_pro_staff_97_v13_2_1_1.jpeg"}
+    {id: 1, nombre: "CLASH 98 V2.0", precio: "S/.989.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr074211u_1_clash_98_v2_rd_bl.png.cq5dam.web.1200.1200_1_1.jpeg"},
+    {id: 2, nombre: "CLASH 108 V2.0", precio: "S/.969.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/70463b1ff005ad550922e9aee1aaa0df/w/r/wr074511u_1_clash_108_v2_rd_bl.png.cq5dam.web.1200.1200.jpeg"},
+    {id: 3, nombre: "CLASH 100 V2.0", precio: "S/.969.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr074011u_1_clash_100_v2_rd_bl.png.cq5dam.web.1200.1200_1_1.jpeg"},
+    {id: 4, nombre: "CLASH 100 PRO V2.0", precio: "S/:969.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr074011u_1_clash_100_v2_rd_bl.png.cq5dam.web.1200.1200_1_1.jpeg"},
+    {id: 5, nombre: "CLASH 100UL V2.0", precio: "S/:889.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr074011u_1_clash_100_v2_rd_bl.png.cq5dam.web.1200.1200_1_1.jpeg"},
+    {id: 6, nombre: "PRO STAFF 97L V13", precio: "S/.955.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr043811u_1_pro_staff_97_v13_2_1_1.jpeg"},
+    {id: 7, nombre: "PRO STAFF 97 CV RAQUETA DE TENIS", precio: "S/. 839.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr043811u_1_pro_staff_97_v13_2_1_1.jpeg"},
+    {id: 8, nombre: "PRO STAFF 97 CV RAQUETA DE TENIS", precio: "S/. 839.00", img: "https://wilsonstore.com.pe/media/catalog/product/cache/97001a7609811bc0c28380f578d7c7df/w/r/wr043811u_1_pro_staff_97_v13_2_1_1.jpeg"}
 ]
